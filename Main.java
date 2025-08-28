05b05b170ddc507ff0b8e3c2e2bbec15e43149dc
Houssein <bahpsl45@gmail.com>
2025-08-28 17:30:08 +0200

    main

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 // Fichier : Main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins pipeline by houssein pour la formation devops!");
+        System.out.println("Hello, Jenkins pipeline by houssein pour la formation devops 2025!");
     }
 }
 
